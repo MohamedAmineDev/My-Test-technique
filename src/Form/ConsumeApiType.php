6f399e33ca29fceb51f5ad63a412af61c0ce0d75
Mohamed Amine BENSAFTA <mohamedamine.bensafta@sesame.com.tn>
2023-09-15 13:19:19 +0100

    Nettoyage du code source php et twig

--- src/Form/ConsumeApiType.php
+++ src/Form/ConsumeApiType.php
@@ -1,14 +1,27 @@
 <?php
+/**
+ * Ce fichier fait partie du projet mon-test-technique
+ *
+ * Dans le cas où le fichier est complexe ou important, ne pas hésiter à donner des détails ici…
+ *
+ * @package Form
+ * @copyright 2023 Quantic Factory
+ */
 
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Cette classe  permet de créer un formulaire pour la consommation d'un api externe
+ *
+ * @author Mohamed Amine Ben Safta <mohamedaminebensafta[@]gmail.com>
+ */
 
 class ConsumeApiType extends AbstractType
 {
